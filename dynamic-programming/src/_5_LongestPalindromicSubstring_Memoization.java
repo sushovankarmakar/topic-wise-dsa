feb815d8075c23cfc0e2a9736a5c856a0d1fe635
Sushovan <officialkarmakar@gmail.com>
2022-06-17 20:08:22 +0530

    _5_LongestPalindromicSubstring : Recursive, Memoization

---
+++ dynamic-programming/src/_5_LongestPalindromicSubstring_Memoization.java
@@ -0,0 +1,51 @@
+package src;
+
+/**
+ * https://leetcode.com/problems/longest-palindromic-substring/
+ *
+ * https://www.youtube.com/watch?v=Msghl9189X4 (Helpful to understand the logic)
+ * https://leetcode.com/problems/longest-palindromic-substring/discuss/1056859/recursion-a-few-dp-variants (I followed this memoization approach)
+ */
+public class _5_LongestPalindromicSubstring_Memoization {
+
+    public static void main(String[] args) {
+        System.out.println(longestPalindrome("aacabdkacaa"));   // aca
+        System.out.println(longestPalindrome("babad")); // bab
+        System.out.println(longestPalindrome("cbbd"));  // bb
+        System.out.println(longestPalindrome("abbcccbbbcaaccbababcbcabca")); // bbcccbb
+    }
+
+    // Memoization + Bottom Up DP
+    public static String longestPalindrome(String input) {
+
+        int n = input.length();
+        String[][] dp = new String[n + 1][n + 1];
+
+        return lps(input, 0, n - 1, dp);
+    }
+
+    private static String lps(String input, int start, int end, String[][] dp) {
+
+        if (start > end) return ""; // base condition
+        if (start == end) return String.valueOf(input.charAt(start));   // base condition
+
+        if (dp[start][end] != null) return dp[start][end];  // memoization
+
+        if (input.charAt(start) == input.charAt(end)) {
+
+            int lpsRemainingStringLength = end - start - 1;
+
+            String middleString = lps(input, start + 1, end - 1, dp);
+
+            if (lpsRemainingStringLength == middleString.length()) {
+
+                return dp[start][end] = input.charAt(start) + middleString + input.charAt(end);
+            }
+        }
+
+        String left  = lps(input, start + 1, end, dp);
+        String right = lps(input, start, end - 1, dp);
+
+        return dp[start][end] = (left.length() > right.length() ? left : right);
+    }
+}
---
+++ dynamic-programming/src/_5_LongestPalindromicSubstring_Recursive.java
@@ -0,0 +1,65 @@
+package src;
+
+/**
+ * https://leetcode.com/problems/longest-palindromic-substring/
+ *
+ * https://www.youtube.com/watch?v=Msghl9189X4 (Helpful to understand the logic)
+ * https://leetcode.com/problems/longest-palindromic-substring/discuss/1056859/recursion-a-few-dp-variants (I followed this recursive approach)
+ */
+/**
+ * Amazon, Microsoft, Google, Adobe, Apple, Facebook, Goldman Sachs, Oracle,
+ * Yahoo, Bloomberg, Visa, tiktok, Uber, Walmart, ByteDance, PayTM
+ */
+public class _5_LongestPalindromicSubstring_Recursive {
+
+    /**
+     * WHAT MISTAKE I DID :
+     * Longest Common Substring :
+     * Reverse of S become S'
+     * Find the longest common substring between S and S' which must also be the longest palindromic substring.
+     * THIS WON'T WORK. example : aacabdkacaa
+     *
+     * We could see that the longest common substring method fails when there exists a reversed copy of
+     * a non-palindromic substring in some other part of S.
+     */
+    public static void main(String[] args) {
+        System.out.println(longestPalindrome("aacabdkacaa"));   // aca
+        System.out.println(longestPalindrome("babad")); // bab
+        System.out.println(longestPalindrome("cbbd"));  // bb
+        System.out.println(longestPalindrome("abbcccbbbcaaccbababcbcabca")); // will take long time, so go for memoization.
+    }
+
+    /**
+     * Similar concept to longest palindromic subsequence.
+     */
+    public static String longestPalindrome(String input) {
+
+        return lps(input, 0, input.length() - 1);
+    }
+
+    private static String lps(String input, int start, int end) {
+
+        if (start > end) return "";
+
+        if (start == end) return String.valueOf(input.charAt(start));
+
+        // if start and end char are same,
+        // then check if remaining string in between start and end char is palindrome or not.
+        if (input.charAt(start) == input.charAt(end)) {
+
+            int lpsRemainingStringLength = end - start - 1;
+
+            String middleString = lps(input, start + 1, end - 1);
+
+            if (lpsRemainingStringLength == middleString.length()) {
+
+                return input.charAt(start) + middleString + input.charAt(end);
+            }
+        }
+
+        String left = lps(input, start + 1, end);
+        String right = lps(input, start, end - 1);
+
+        return left.length() > right.length() ? left : right;
+    }
+}
