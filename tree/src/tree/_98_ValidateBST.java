426e53f0589f8839ee242d8fda10868cabc30afa
Sushovan <officialkarmakar@gmail.com>
2022-08-11 23:39:39 +0530

    _98_ValidateBST

---
+++ tree/src/tree/_98_ValidateBST.java
@@ -0,0 +1,29 @@
+package tree;
+
+/**
+ * https://practice.geeksforgeeks.org/problems/check-for-bst/1
+ * https://leetcode.com/problems/validate-binary-search-tree/
+ *
+ * https://www.youtube.com/watch?v=f-sj7I5oXEI (Striver)
+ * https://www.geeksforgeeks.org/a-program-to-check-if-a-binary-tree-is-bst-or-not/
+ */
+public class _98_ValidateBST {
+
+    /**
+     * [5,4,6,null,null,3,7] - I made this mistake.
+     */
+    public boolean isValidBST(TreeNode root) {
+
+        return validate(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    private boolean validate(TreeNode root, long minRange, long maxRange) {
+
+        if (root == null) return true;
+
+        return (minRange < root.val)
+                && (root.val < maxRange)
+                && validate(root.left, minRange, root.val)
+                && validate(root.right, root.val, maxRange);
+    }
+}
