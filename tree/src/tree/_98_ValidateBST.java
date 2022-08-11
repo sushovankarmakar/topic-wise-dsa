0d1920ab93a6324e32e3ee9f76f74206b095778a
Sushovan <officialkarmakar@gmail.com>
2022-08-11 23:41:58 +0530

    _98_ValidateBST : added intuition

--- tree/src/tree/_98_ValidateBST.java
+++ tree/src/tree/_98_ValidateBST.java
@@ -3,7 +3,7 @@ package tree;
 /**
  * https://practice.geeksforgeeks.org/problems/check-for-bst/1
  * https://leetcode.com/problems/validate-binary-search-tree/
- *
+ * <p>
  * https://www.youtube.com/watch?v=f-sj7I5oXEI (Striver)
  * https://www.geeksforgeeks.org/a-program-to-check-if-a-binary-tree-is-bst-or-not/
  */
@@ -13,6 +13,10 @@ public class _98_ValidateBST {
      * [5,4,6,null,null,3,7] - I made this mistake.
      */
     public boolean isValidBST(TreeNode root) {
+
+        // Intuition :
+        // it's all about checking each value within a given range or not.
+        // if it is, then recursively call check for the left and right children.
 
         return validate(root, Long.MIN_VALUE, Long.MAX_VALUE);
     }
