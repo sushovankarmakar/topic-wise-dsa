228f516af5b05e11c55e61db3ec4ba09b27cb94a
Sushovan <officialkarmakar@gmail.com>
2022-08-19 20:09:43 +0530

    Max Sum SubArray Of Size K

--- sliding-window/src/fixed_size/MaxSumSubArrayOfSizeK.java
+++ sliding-window/src/fixed_size/MaxSumSubArrayOfSizeK.java
@@ -4,7 +4,7 @@ package fixed_size;
  * https://practice.geeksforgeeks.org/problems/max-sum-subarray-of-size-k5313/1
  * <p>
  * https://www.youtube.com/watch?v=KtpqeN0Goro&ab_channel=AdityaVerma (BEST)
- *
+ * <p>
  * https://www.callicoder.com/maximum-sum-subarray-of-size-k/ (Good explanation)
  */
 
@@ -15,6 +15,7 @@ public class MaxSumSubArrayOfSizeK {
         System.out.println(getMaxSumSubArray(arr, 2));  // 700
 
         System.out.println(getMaxSumSubArray(new int[]{2, 5, 1, 8, 2, 9, 1}, 3));   // 19
+        System.out.println(getMaxSumSubArray_2(new int[]{2, 5, 1, 8, 2, 9, 1}, 3));   // 19
     }
 
     private static int getMaxSumSubArray(int[] arr, int k) {
@@ -34,6 +35,35 @@ public class MaxSumSubArrayOfSizeK {
 
                 currSum -= arr[left];
                 left++;
+            }
+        }
+        return maxSum;
+    }
+
+    // this method of code is easy to visualize.
+    private static int getMaxSumSubArray_2(int[] arr, int k) {
+        int n = arr.length;
+        int maxSum = Integer.MIN_VALUE;
+
+        int left = 0, right = 0;
+        int currSum = 0;
+        int window;
+
+        while (right < n) {
+
+            currSum += arr[right];
+            window = right - left + 1; // right - left + 1 <- is the window size.
+
+            if (window < k) {
+                right++;
+            } else if (window == k) {
+                maxSum = Math.max(maxSum, currSum);
+
+                currSum -= arr[left];
+
+                // now slide the window
+                left++;
+                right++;
             }
         }
         return maxSum;
