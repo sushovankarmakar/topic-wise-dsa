1dead833f7a0d40c4a4fa0e2d1b7031f9b51b3bb
Sushovan <officialkarmakar@gmail.com>
2022-08-19 20:13:05 +0530

    First Negative Num In Window Of Size K

--- sliding-window/src/fixed_size/FirstNegativeNumInWindowOfSizeK.java
+++ sliding-window/src/fixed_size/FirstNegativeNumInWindowOfSizeK.java
@@ -4,10 +4,11 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 /**
- * https://practice.geeksforgeeks.org/problems/first-negative-integer-in-every-window-of-size-k3345/1#
+ * https://practice.geeksforgeeks.org/problems/first-negative-integer-in-every-window-of-size-k3345/1
+ * https://www.geeksforgeeks.org/first-negative-integer-every-window-size-k/
  * <p>
  * https://www.youtube.com/watch?v=uUXXEgK2Jh8&ab_channel=AdityaVerma
- *
+ * <p>
  * https://www.callicoder.com/first-negative-number-in-every-window-of-size-k/
  */
 public class FirstNegativeNumInWindowOfSizeK {
@@ -48,6 +49,50 @@ public class FirstNegativeNumInWindowOfSizeK {
                 left++;
             }
         }
+        return negNums;
+    }
+
+    /**
+     * Below code is much easier to visualize.
+     */
+    private static long[] printFirstNegativeInteger(long[] arr, int n, int k) {
+
+        Queue<Long> queue = new LinkedList<>();
+        int i = 0, j = 0;
+        int windowSize = 0;
+        long[] negNums = new long[n - k + 1];
+        int idx = 0;
+
+        while (j < n) {
+
+            if (arr[j] < 0) {
+                queue.add(arr[j]);
+            }
+
+            windowSize = j - i + 1;
+
+            if (windowSize < k) {
+                j++;
+
+            } else if (windowSize == k) {
+
+                if (!queue.isEmpty()) {
+
+                    long currNum = queue.peek();
+                    negNums[idx++] = currNum;
+
+                    if (currNum == arr[i]) {
+                        queue.remove();
+                    }
+
+                } else {
+                    negNums[idx++] = 0;
+                }
+                i++;
+                j++;
+            }
+        }
+
         return negNums;
     }
 }
