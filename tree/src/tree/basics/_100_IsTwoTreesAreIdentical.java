a665e0b1ccc5337aafbc4c8ee77d23563b5e271e
Sushovan <officialkarmakar@gmail.com>
2022-08-30 21:53:51 +0530

    100 Is Two Trees Are Identical

---
+++ tree/src/tree/basics/_100_IsTwoTreesAreIdentical.java
@@ -0,0 +1,28 @@
+package tree.basics;
+
+import tree.TreeNode;
+
+/**
+ * https://leetcode.com/problems/same-tree/
+ * https://practice.geeksforgeeks.org/problems/determine-if-two-trees-are-identical/1
+ * <p>
+ * https://www.youtube.com/watch?v=BhuvF_-PWS0
+ */
+public class _100_IsTwoTreesAreIdentical {
+
+    /**
+     * time  : O(n)
+     * space : O(n)
+     * <p>
+     * traverse both the trees simultaneously.
+     */
+    boolean isIdentical(TreeNode root1, TreeNode root2) {
+
+        if (root1 == null && root2 == null) return true;
+        if (root1 == null || root2 == null) return false;
+
+        return root1.val == root2.val
+                && isIdentical(root1.left, root2.left)
+                && isIdentical(root1.right, root2.right);
+    }
+}
