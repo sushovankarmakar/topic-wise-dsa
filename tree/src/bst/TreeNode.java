765a21d501ae0e8c3bd7c7d64a4f148fc143e2bd
Sushovan <officialkarmakar@gmail.com>
2022-09-12 23:06:16 +0530

    tree node

---
+++ tree/src/bst/TreeNode.java
@@ -0,0 +1,105 @@
+package bst;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+// https://www.geeksforgeeks.org/tree-traversals-inorder-preorder-and-postorder/
+
+/**
+ * to solve a tree problem in a recursion. 3 things to keep in mind.
+ * <p>
+ * 1. the definition of recursion function (could be the same as the original one, or different)
+ * 2. with the subtree returned results, what else you want to do for current level?
+ * 3. what value to return to the upper level?
+ * <p>
+ * 60% tree problem can be solved this way. After you have a very deep understanding of this, you can solve all such problem.
+ */
+
+public class TreeNode {
+
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    TreeNode() {
+    }
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+
+    public static TreeNode buildTree(String str) {
+
+        if (str.length() == 0 || str.charAt(0) == 'N') {
+            return null;
+        }
+
+        String[] ip = str.split("\\s+");
+
+        // Create the root of the tree
+        TreeNode root = new TreeNode(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+
+        Queue<TreeNode> queue = new LinkedList<>();
+
+        queue.add(root);
+        // Starting from the second element
+
+        int i = 1;
+        while (queue.size() > 0 && i < ip.length) {
+
+            // Get and remove the front of the queue
+            TreeNode currNode = queue.peek();
+            queue.remove();
+
+            // Get the current node's value from the string
+            String currVal = ip[i];
+
+            // If the left child is not null
+            if (!currVal.equals("N")) {
+
+                // Create the left child for the current node
+                currNode.left = new TreeNode(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+
+            // For the right child
+            i++;
+            if (i >= ip.length)
+                break;
+
+            currVal = ip[i];
+
+            // If the right child is not null
+            if (!currVal.equals("N")) {
+
+                // Create the right child for the current node
+                currNode.right = new TreeNode(Integer.parseInt(currVal));
+
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+
+        return root;
+    }
+
+    static void printInorder(TreeNode root) {
+        if (root == null)
+            return;
+
+        printInorder(root.left);
+        System.out.print(root.val + " ");
+
+        printInorder(root.right);
+    }
+
+}
