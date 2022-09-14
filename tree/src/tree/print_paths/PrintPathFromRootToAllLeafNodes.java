6ea0d85357b64588940474e397bd6b7146f328c6
Sushovan <officialkarmakar@gmail.com>
2022-09-14 10:03:22 +0530

    1. print path from root to all leaf nodes. 2. print path from root to a given node.

---
+++ tree/src/tree/print_paths/PrintPathFromRootToAllLeafNodes.java
@@ -0,0 +1,100 @@
+package tree.print_paths;
+
+import tree.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * https://practice.geeksforgeeks.org/problems/root-to-leaf-paths/1
+ * <p>
+ * https://www.youtube.com/watch?v=4cvz2sfKNDA (Coding Simplified : Learn the concept)
+ * https://discuss.geeksforgeeks.org/comment/48097b48e4de77dae34197475601ba61 (Followed this code)
+ */
+public class PrintPathFromRootToAllLeafNodes {
+
+    public static void main(String[] args) {
+        TreeNode root1 = TreeNode.buildTree("10 20 30 40 60 N N");  // [[10, 20, 40], [10, 20, 60], [10, 30]]
+        System.out.println(getPaths(root1));
+
+        TreeNode root2 = TreeNode.buildTree("1 2 3");   // [[1, 2], [1, 3]]
+        System.out.println(getPaths(root2));
+
+        TreeNode root3 = TreeNode.buildTree("7 2 N 3 5 N 4 4 N 3 6 N 2 5 N 3 7 N 7 8 N 1 6 N 5 2 N 6 7");
+        System.out.println(getPaths(root3));
+    }
+
+    private static List<List<Integer>> paths;
+
+    /**
+     * Short and Crispy
+     * https://discuss.geeksforgeeks.org/comment/48097b48e4de77dae34197475601ba61
+     * <p>
+     * ----------------------
+     * Points to remember :
+     * 1. IMPORTANT to keep the remove operation at last after visiting both the children.
+     * MISTAKE I make : I kept remove operation after visiting left child only.
+     * ----------------------
+     * 2. IMPORTANT to add a copy of the current list, or else while removing it will remove from the same list.
+     * ----------------------
+     */
+    private static List<List<Integer>> getPaths(TreeNode root) {
+        paths = new ArrayList<>();
+        findPaths(root, new ArrayList<>());
+        return paths;
+    }
+
+    /**
+     * PreOrder traversal : root, left, right
+     */
+    private static void findPaths(TreeNode root, List<Integer> path) {
+
+        if (root == null) return;
+
+        path.add(root.val);
+        if (root.left == null && root.right == null) {
+            // IMPORTANT to add a copy of the current list, or else while removing it will remove from the same list.
+            paths.add(new ArrayList<>(path));
+
+            // IMPORTANT : Don't add Return statement here.
+        }
+
+        findPaths(root.left, path);
+        findPaths(root.right, path);
+
+        // IMPORTANT to keep the remove operation at last after visiting both the children.
+        // MISTAKE : I kept remove operation after visiting left child only.
+        path.remove(path.size() - 1);   // while going back remove, last element from arrayList
+    }
+
+    /**
+     * --------------------------------------------------------------------------------------------------------------
+     * Lengthy code. First I wrote this.
+     * This solution use index, but above solution don't use it.
+     */
+    public static List<List<Integer>> getPaths_Old(TreeNode root) {
+        paths = new ArrayList<>();
+        findPaths_Old(root, new ArrayList<>(), 0);
+        return paths;
+    }
+
+    private static void findPaths_Old(TreeNode root, ArrayList<Integer> path, int index) {
+
+        if (root == null) return;
+
+        if (root.left == null && root.right == null) {
+            path.add(root.val);
+            paths.add(new ArrayList<>(path));
+            return;
+        }
+
+        path.add(index, root.val);
+        findPaths_Old(root.left, path, index + 1);
+
+        for (int i = path.size() - 1; i > index; i--) {
+            path.remove(i);
+        }
+
+        findPaths_Old(root.right, path, index + 1);
+    }
+}
---
+++ tree/src/tree/print_paths/PrintPathFromRootToGivenNode.java
@@ -0,0 +1,58 @@
+package tree.print_paths;
+
+import tree.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * https://www.interviewbit.com/problems/path-to-given-node/
+ * https://discuss.geeksforgeeks.org/comment/48097b48e4de77dae34197475601ba61 (Followed this code)
+ * <p>
+ * Similar Question :
+ * https://practice.geeksforgeeks.org/problems/root-to-leaf-paths/1
+ * <p>
+ * https://www.geeksforgeeks.org/print-path-root-given-node-binary-tree/ (A bit different explanation - I didn't follow this)
+ * https://www.youtube.com/watch?v=fmflMqVOC7k (Striver : Same explanation as GFG article)
+ */
+public class PrintPathFromRootToGivenNode {
+
+    public static void main(String[] args) {
+        TreeNode root1 = TreeNode.buildTree("10 20 30 40 60 N N");
+        System.out.println(getPaths(root1, 60));    // [10, 20, 60]
+
+        TreeNode root2 = TreeNode.buildTree("1 2 3 4 5 6 7");
+        System.out.println(getPaths(root2, 5));    // [1, 2, 5]
+    }
+
+    private static List<Integer> targetPath;
+
+    private static List<Integer> getPaths(TreeNode root, int target) {
+        targetPath = new ArrayList<>();
+        findPaths(root, new ArrayList<>(), target);
+        return targetPath;
+    }
+
+    /**
+     * PreOrder traversal : root, left, right
+     */
+    private static void findPaths(TreeNode root, List<Integer> path, int target) {
+
+        if (root == null) return;
+
+        path.add(root.val);
+        if (root.val == target) {
+            // IMPORTANT to add a copy of the current list, or else while removing it will remove from the same list.
+            targetPath.addAll(path);
+
+            // IMPORTANT : Don't add Return statement here.
+        }
+
+        findPaths(root.left, path, target);
+        findPaths(root.right, path, target);
+
+        // IMPORTANT to keep the remove operation at last after visiting both the children.
+        // MISTAKE : I kept remove operation after visiting left child only.
+        path.remove(path.size() - 1);   // while going back remove, last element from arrayList
+    }
+}
