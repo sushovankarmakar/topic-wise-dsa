45a718bbf5274a62c662531a2635c7d2deca6d81
Sushovan <officialkarmakar@gmail.com>
2022-10-24 13:43:12 +0530

    construct trees from 1. inorder and preorder 2. inorder and postorder

---
+++ tree/src/tree/contruct_trees/_105_ConstructBinaryTreeFromPreorderAndInorder.java
@@ -0,0 +1,56 @@
+package tree.contruct_trees;
+
+import tree.TreeNode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
+ * https://www.youtube.com/watch?v=aZNaLrVebKQ
+ */
+public class _105_ConstructBinaryTreeFromPreorderAndInorder {
+
+    public static void main(String[] args) {
+        int[] preOrder = new int[]{3, 9, 20, 15, 7};
+        int[] inOrder = new int[]{9, 3, 15, 20, 7};
+        TreeNode root = buildTree(preOrder, inOrder);
+        TreeNode.printLevelOrder(root);    // 3 | 9 20 | 15 7
+    }
+
+    private static final Map<Integer, Integer> inOrderValueIndexMap = new HashMap<>();
+    private static int preOrderRootIndex = 0;
+
+    private static TreeNode buildTree(int[] preOrder, int[] inOrder) {
+
+        for (int i = 0; i < inOrder.length; i++) {
+            inOrderValueIndexMap.put(inOrder[i], i);
+        }
+
+        return build(preOrder, inOrder, 0, inOrder.length - 1);
+    }
+
+    private static TreeNode build(int[] preOrder, int[] inOrder, int inOrderStartIndex, int inOrderEndIndex) {
+
+        if (inOrderStartIndex > inOrderEndIndex) return null;
+
+        TreeNode root = new TreeNode(preOrder[preOrderRootIndex]);
+        preOrderRootIndex++;
+
+        if (root == null || inOrderStartIndex == inOrderEndIndex) return root;
+
+        int inOrderRootIndex = inOrderValueIndexMap.get(root.val);
+
+        /**
+         * In case of PreOrder and InOrder,
+         * we will first construct left child and then right child
+         *
+         * In case of PostOrder and InOrder,
+         * we will first construct right child and then left child
+         */
+        root.left = build(preOrder, inOrder, inOrderStartIndex, inOrderRootIndex - 1);
+        root.right = build(preOrder, inOrder, inOrderRootIndex + 1, inOrderEndIndex);
+
+        return root;
+    }
+}
---
+++ tree/src/tree/contruct_trees/_106_ConstructBinaryTreeFromPostorderAndInorder.java
@@ -0,0 +1,61 @@
+package tree.contruct_trees;
+
+import tree.TreeNode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * https://leetcode.com/problems/construct-binary-tree-from-inorder-and-postorder-traversal/
+ * https://www.youtube.com/watch?v=LgLRTaEMRVc
+ */
+public class _106_ConstructBinaryTreeFromPostorderAndInorder {
+
+    public static void main(String[] args) {
+        int[] postOrder = new int[]{9, 15, 7, 20, 3};
+        int[] inOrder = new int[]{9, 3, 15, 20, 7};
+        TreeNode root = buildTree(postOrder, inOrder);
+        TreeNode.printLevelOrder(root);    // 3 | 9 20 | 15 7
+    }
+
+    private static final Map<Integer, Integer> inOrderValueIndexMap = new HashMap<>();
+    private static int postOrderRootIndex = 0;
+
+    private static TreeNode buildTree(int[] postOrder, int[] inOrder) {
+
+        for (int i = 0; i < inOrder.length; i++) {
+            inOrderValueIndexMap.put(inOrder[i], i);
+        }
+
+        /**
+         * we start from the end cause in postOrder, root resides at the end
+         */
+        postOrderRootIndex = postOrder.length - 1;
+
+        return build(postOrder, inOrder, 0, inOrder.length - 1);
+    }
+
+    private static TreeNode build(int[] postOrder, int[] inOrder, int inOrderStartIndex, int inOrderEndIndex) {
+
+        if (inOrderStartIndex > inOrderEndIndex) return null;
+
+        TreeNode root = new TreeNode(postOrder[postOrderRootIndex]);
+        postOrderRootIndex--;
+
+        if (root == null || inOrderStartIndex == inOrderEndIndex) return root;
+
+        int inOrderRootIndex = inOrderValueIndexMap.get(root.val);
+
+        /**
+         * In case of PreOrder and InOrder,
+         * we will first construct left child and then right child
+         *
+         * In case of PostOrder and InOrder,
+         * we will first construct right child and then left child
+         */
+        root.right = build(postOrder, inOrder, inOrderRootIndex + 1, inOrderEndIndex);
+        root.left = build(postOrder, inOrder, inOrderStartIndex, inOrderRootIndex - 1);
+
+        return root;
+    }
+}
