11f4c1a7f42320ca56bc52407796db6e68ecfa21
Sushovan <officialkarmakar@gmail.com>
2022-10-27 15:16:47 +0530

    1. Find a value in BST. 2. Find the closest value in BST.

---
+++ tree/src/bst/_700_SearchInBST.java
@@ -0,0 +1,37 @@
+package bst;
+
+/**
+ * https://leetcode.com/problems/search-in-a-binary-search-tree/
+ * https://practice.geeksforgeeks.org/problems/search-a-node-in-bst/1
+ * <p>
+ * https://www.youtube.com/watch?v=KcNt6v_56cc
+ */
+public class _700_SearchInBST {
+
+    private TreeNode searchBST_iterative(TreeNode root, int val) {
+
+        while (root != null && root.val != val) {
+            root = val < root.val ? root.left : root.right;
+        }
+
+        return root;
+    }
+
+    private TreeNode searchBST(TreeNode root, int val) {
+
+        if (root == null || root.val == val) return root;
+
+        return val < root.val ? searchBST(root.left, val) : searchBST(root.right, val);
+    }
+
+    // GFG Solution.
+    boolean search(TreeNode root, int x) {
+
+        while (root != null && root.val != x) {
+
+            root = x < root.val ? root.left : root.right;
+        }
+
+        return root != null;
+    }
+}
---
+++ tree/src/bst/floor_and_ceil/_270_FindTheClosestElementInBST.java
@@ -0,0 +1,93 @@
+package bst.floor_and_ceil;
+
+import bst.TreeNode;
+
+/**
+ * https://practice.geeksforgeeks.org/problems/find-the-closest-element-in-bst/1  (asks the absolute difference)
+ * https://leetcode.com/problems/closest-binary-search-tree-value/ (asks the actual value)
+ * <p>
+ * https://practice.geeksforgeeks.org/problems/implementing-ceil-in-bst/1 (Find ceil)
+ * <p>
+ * https://www.geeksforgeeks.org/floor-and-ceil-from-a-bst/ (BEST Article. I followed the iterative solution.)
+ * <p>
+ * https://www.youtube.com/watch?v=KSsk8AhdOZA (Find Ceil - Striver)
+ * https://www.youtube.com/watch?v=xm_W1ub-K-w (Find Floor - Striver)
+ */
+public class _270_FindTheClosestElementInBST {
+
+    /**
+     * Floor Value Node:
+     * ----------------
+     * Node with the greatest data lesser than or equal to the key value.
+     *
+     * Ceil Value Node:
+     * ---------------
+     * Node with the smallest data larger than or equal to the key value.
+     *
+     * Find the floor and ceil for the given target.
+     */
+
+
+    /**
+     * GFG Variation.
+     */
+    private static int minDiff(TreeNode root, int target) {
+
+        int floor = -1;
+        int ceil = -1;
+
+        while (root != null) {
+
+            if (root.val == target) {
+                return 0;
+            }
+
+            if (target < root.val) {
+                ceil = root.val;   // as of now, this is the nearest bigger value I've seen.
+                root = root.left;   // let's explore more on the left subtree, because on the right, I'll get only bigger values which is useless for me.
+
+            } else {    // root.data < target
+
+                floor = root.val;  // as of now, this is the nearest smaller value I've seen.
+                root = root.right;  // let's explore more on the right subtree, because on the left, I'll get only smaller values which is useless for me.
+            }
+        }
+
+        if (floor == -1) return Math.abs(target - ceil);
+        if (ceil == -1) return Math.abs(target - floor);
+
+        return Math.min(Math.abs(target - ceil), Math.abs(target - floor));
+    }
+
+
+    /**
+     * Leetcode Variation.
+     */
+    public int closestValue(TreeNode root, double target) {
+
+        int floor = -1;
+        int ceil = -1;
+
+        while (root != null) {
+
+            if (root.val == target) {
+                return root.val;
+            }
+
+            if (target < root.val) {
+                ceil = root.val;   // as of now, this is the nearest bigger value I've seen.
+                root = root.left;   // let's explore more on the left subtree, because on the right, I'll get only bigger values which is useless for me.
+
+            } else {    // root.data < target
+
+                floor = root.val;  // as of now, this is the nearest smaller value I've seen.
+                root = root.right; // let's explore more on the right subtree, because on the left, I'll get only smaller values which is useless for me.
+            }
+        }
+
+        if (floor == -1) return ceil;
+        if (ceil == -1) return floor;
+
+        return Math.abs(target - ceil) > Math.abs(target - floor) ? floor : ceil;
+    }
+}
