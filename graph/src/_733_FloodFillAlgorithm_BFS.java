e334f1c2e7a56093b46ff8a2b9b7eeaf6279f1fd
Sushovan <officialkarmakar@gmail.com>
2023-02-02 12:43:22 +0530

    _733_FloodFillAlgorithm 1. BFS 2. DFS

---
+++ graph/src/_733_FloodFillAlgorithm_BFS.java
@@ -0,0 +1,70 @@
+package src;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * https://www.youtube.com/watch?v=C-2_uSRli8o (Striver) - He solved it using DFS. I did using BFS.
+ *
+ * https://leetcode.com/problems/flood-fill/
+ * https://practice.geeksforgeeks.org/problems/flood-fill-algorithm1856/1
+ */
+public class _733_FloodFillAlgorithm_BFS {
+
+    class Pair {
+        int row;
+        int col;
+
+        Pair(int row, int col) {
+            this.row = row;
+            this.col = col;
+        }
+    }
+
+    /**
+     * time  : O(M * N)
+     * space : O(M * N)
+     */
+    private int[][] fourDirections = {{0, -1}, {-1, 0}, {0, 1}, {1, 0}};
+
+    public int[][] floodFill(int[][] image, int startRow, int startCol, int newColor) {
+
+        int n = image.length;
+        int m = image[0].length;
+
+        boolean[][] isVisited = new boolean[n][m];
+
+        Queue<Pair> queue = new LinkedList<>();
+        queue.add(new Pair(startRow, startCol));
+        isVisited[startRow][startCol] = true;
+
+        int startingColor = image[startRow][startCol];
+
+        while (!queue.isEmpty()) {
+
+            Pair currPixel = queue.poll();
+            image[currPixel.row][currPixel.col] = newColor;
+
+            for (int i = 0; i < fourDirections.length; i++) {
+
+                int newRow = currPixel.row + fourDirections[i][0];
+                int newCol = currPixel.col + fourDirections[i][1];
+
+                if (isValidPixel(newRow, newCol, image, startingColor, isVisited)) {
+
+                    queue.add(new Pair(newRow, newCol));
+                    isVisited[newRow][newCol] = true;
+                }
+            }
+        }
+        return image;
+    }
+
+    private boolean isValidPixel(int newRow, int newCol, int[][] image, int startingColor,
+                                 boolean[][] isVisited) {
+
+        return newRow >= 0 && newCol >= 0 &&
+                newRow < image.length && newCol < image[0].length &&
+                image[newRow][newCol] == startingColor && !isVisited[newRow][newCol];
+    }
+}
---
+++ graph/src/_733_FloodFillAlgorithm_DFS.java
@@ -0,0 +1,55 @@
+package src;
+
+/**
+ * https://www.youtube.com/watch?v=C-2_uSRli8o (Striver) - He solved it using DFS. I did using BFS.
+ *
+ * https://leetcode.com/problems/flood-fill/
+ * https://practice.geeksforgeeks.org/problems/flood-fill-algorithm1856/1
+ */
+public class _733_FloodFillAlgorithm_DFS {
+
+    private int[][] fourDirections = {{0, -1}, {-1, 0}, {0, 1}, {1, 0}};
+
+    /**
+     * time  : O(M * N)
+     * space : O(M * N)
+     */
+    public int[][] floodFill(int[][] image, int startRow, int startCol, int newColor) {
+
+        int n = image.length;
+        int m = image[0].length;
+
+        boolean[][] isVisited = new boolean[n][m];
+
+        int startingColor = image[startRow][startCol];
+
+        dfs(image, startRow, startCol, startingColor, newColor, isVisited);
+        return image;
+    }
+
+    private void dfs(int[][] image, int currRow, int currCol, int startingColor, int newColor,
+                     boolean[][] isVisited) {
+
+        image[currRow][currCol] = newColor;
+
+        for (int i = 0; i < fourDirections.length; i++) {
+
+            int newRow = currRow - fourDirections[i][0];
+            int newCol = currCol - fourDirections[i][1];
+
+            if (isValidPixel(newRow, newCol, image, startingColor, isVisited)) {
+
+                isVisited[newRow][newCol] = true;
+                dfs(image, newRow, newCol, startingColor, newColor, isVisited);
+            }
+        }
+    }
+
+    private boolean isValidPixel(int newRow, int newCol, int[][] image, int startingColor,
+                                 boolean[][] isVisited) {
+
+        return newRow >= 0 && newCol >= 0 &&
+                newRow < image.length && newCol < image[0].length &&
+                image[newRow][newCol] == startingColor && !isVisited[newRow][newCol];
+    }
+}
