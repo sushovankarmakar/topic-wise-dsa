f6cd67908fbe13b6fd495985036fc74f2ecb2220
Sushovan <officialkarmakar@gmail.com>
2024-02-25 08:42:51 +0530

    BFS traversal

--- graph/src/traversal/BFS.java
+++ graph/src/traversal/BFS.java
@@ -1,9 +1,6 @@
 package src.traversal;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
 
 /**
  * https://www.youtube.com/watch?v=-tgVpUgsQ5k (Striver)
@@ -13,16 +10,70 @@ import java.util.Queue;
  */
 public class BFS {
 
+    public static void main(String[] args) {
+
+        List<List<Integer>> adjList = new ArrayList<>();
+        adjList.add(Arrays.asList());
+        adjList.add(Arrays.asList(2, 6));
+        adjList.add(Arrays.asList(1, 3, 4));
+        adjList.add(Arrays.asList(2));
+        adjList.add(Arrays.asList(2, 5));
+        adjList.add(Arrays.asList(4, 7));
+        adjList.add(Arrays.asList(1, 7, 8));
+        adjList.add(Arrays.asList(6, 5));
+        adjList.add(Arrays.asList(6));
+
+        /**
+         * Adjacency List
+         * --------------
+         * 1 -> [2, 6]
+         * 2 -> [1, 3, 4]
+         * 3 -> [2]
+         * 4 -> [2, 5]
+         * 5 -> [4, 7]
+         * 6 -> [1, 7, 8]
+         * 7 -> [6, 5]
+         * 8 -> [6]
+         */
+
+        List<Integer> bfs = doBFS(8, adjList);
+        System.out.println(bfs);
+
+        /**
+         * bfs :
+         * ---
+         * 1
+         * 2 6
+         * 3 4 7 8
+         * 5
+         */
+    }
+
+    /**
+     * Concept
+     *
+     * - initialization 3 things :
+     *     - a boolean visited array,
+     *     - a queue and
+     *     - a array-list to store the bfs
+     * - start from a node,
+     *     - mark it visited and
+     *     - add it to bfs list
+     * - now explore it neighbours adjacency list and
+     *     - add them into queue and
+     *     - mark them visited.
+     */
+
     /**
      * space : O(3N)        -> queue + visited array + output array.
      * time : O(N) + O(2E)
      */
     private static List<Integer> doBFS(int numOfNodes, List<List<Integer>> adjList) {
-        boolean[] isVisited = new boolean[numOfNodes];
+        boolean[] isVisited = new boolean[numOfNodes + 1];
         Queue<Integer> queue = new LinkedList<>();
         List<Integer> bfs = new ArrayList<>();
 
-        int currNode = 0;
+        int currNode = 1;   // starting node can be 0 or 1
         isVisited[currNode] = true;
         queue.add(currNode);
 
