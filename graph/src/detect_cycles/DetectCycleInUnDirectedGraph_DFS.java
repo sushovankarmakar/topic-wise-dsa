ca316dff72ed9d3f648f7b94be16d1943293de18
Sushovan <officialkarmakar@gmail.com>
2024-03-05 05:50:48 +0530

    Detect cycle in undirected graph (dfs)

--- graph/src/detect_cycles/DetectCycleInUnDirectedGraph_DFS.java
+++ graph/src/detect_cycles/DetectCycleInUnDirectedGraph_DFS.java
@@ -1,6 +1,6 @@
 package src.detect_cycles;
 
-import java.util.ArrayList;
+import java.util.List;
 
 /**
  * https://www.youtube.com/watch?v=zQ3zgFypzX4
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 public class DetectCycleInUnDirectedGraph_DFS {
 
     // Function to detect cycle in an undirected graph.
-    public boolean isCycle(int v, ArrayList<ArrayList<Integer>> adj) {
+    public boolean isCycle(int v, List<List<Integer>> adj) {
 
         boolean[] isVisited = new boolean[v];
 
@@ -25,22 +25,27 @@ public class DetectCycleInUnDirectedGraph_DFS {
         return false;
     }
 
-    private boolean dfs(int currNode, int parentNode, boolean[] isVisited,
-                        ArrayList<ArrayList<Integer>> adj) {
+    private boolean dfs(int currNode, int parentNode, boolean[] isVisited, List<List<Integer>> adj) {
 
         isVisited[currNode] = true;
 
         for (int adjNode : adj.get(currNode)) {
 
-            if (parentNode != adjNode) {
+            // if adjNode is not parentNode and still it is already visited, then cycle hain.
 
-                // if adjNode is not parentNode and still it is already visited, then cycle hain.
-                if (isVisited[adjNode]) {
+            if (isVisited[adjNode] && adjNode != parentNode) {
+                // if any adj node is already visited but this node is not parent node
+                // then this adj node has been touched by other nodes
+                // that means we can reach this adj node from two different directions
+                // so there is a cycle.
+                return true;    // cycle is present, so return true;
+            }
+
+            if (!isVisited[adjNode]) {
+                isVisited[adjNode] = true;
+
+                if (dfs(adjNode, currNode, isVisited, adj)) {
                     return true;
-                } else {
-                    if (dfs(adjNode, currNode, isVisited, adj)) {
-                        return true;
-                    }
                 }
             }
         }
