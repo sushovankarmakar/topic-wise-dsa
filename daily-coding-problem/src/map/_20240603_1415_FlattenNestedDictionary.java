5c01eef6efa6d2fff6a7686ea258e5fc96480cd1
sushovan.karmakar <officialkarmakar@gmail.com>
2024-06-03 08:51:33 +0530

    1415 : FlattenNestedDictionary : Added solution using java 8 streams

--- daily-coding-problem/src/map/_20240603_1415_FlattenNestedDictionary.java
+++ daily-coding-problem/src/map/_20240603_1415_FlattenNestedDictionary.java
@@ -2,6 +2,8 @@ package src.map;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class _20240603_1415_FlattenNestedDictionary {
 
@@ -24,7 +26,7 @@ public class _20240603_1415_FlattenNestedDictionary {
             Object val = entry.getValue(); // 5
             String newKey = prefix.isBlank() ? key : prefix + "." + key; // example -> prefix : foo, key : a, newKey : foo.a
 
-            if (val instanceof Map<?,?>) {
+            if (val instanceof Map<?, ?>) {
                 @SuppressWarnings("unchecked")
                 Map<String, Object> subMap = (Map<String, Object>) val;
                 flattenHelper(newKey, subMap, flatMap);
@@ -32,6 +34,24 @@ public class _20240603_1415_FlattenNestedDictionary {
                 flatMap.put(newKey, val); // WRONG : flatMap.put(key, val); - WRONG. we should put values in flatMap using only newKey. Dry run to understand it.
             }
         }
+    }
+
+    // https://g.co/gemini/share/a80c982c94d6
+    // Below function does the same thing as the above function
+    // Java 8 function to flatten a nested dictionary
+    public static Map<String, Object> flattenDictionary(Map<String, Object> map, String prefix) {
+        return map.entrySet().stream()
+                .flatMap(entry -> {
+                    Object value = entry.getValue();
+                    if (value instanceof Map) {
+                        return ((Map<String, Object>) value).entrySet()
+                                .stream()
+                                .map(subEntry -> Map.entry(prefix + entry.getKey() + "." + subEntry.getKey(), subEntry.getValue()));
+                    } else {
+                        return Stream.of(Map.entry(prefix + entry.getKey(), value));
+                    }
+                })
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
     }
 
     public static void main(String[] args) {
@@ -47,7 +67,9 @@ public class _20240603_1415_FlattenNestedDictionary {
 
         nestedDict.put("foo", fooMap);
 
+        System.out.println("USING MY OWN FUNCTIONS");
         Map<String, Object> flatMap = flatten(nestedDict);
         flatMap.forEach((k, v) -> System.out.println(k + ": " + v));
+        System.out.println("----------------------");
     }
 }
