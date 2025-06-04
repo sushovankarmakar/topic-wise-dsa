8bb8a7d621207f5a646caa36756eafec49a5b77f
Sushovan Karmakar <sushovankarmakar1996@gmail.com>
2025-06-05 01:48:49 +0530

    Graph: topo sort using BFS and DFS

---
+++ graph/src/topo_sort/basics/_1_TopologicalSort_BFS.java
@@ -0,0 +1,89 @@
+package src.topo_sort.basics;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * https://www.geeksforgeeks.org/problems/topological-sort/1
+ * <p>
+ * https://www.youtube.com/watch?v=73sneFXuTEg&ab_channel=takeUforward (Striver)
+ * https://www.geeksforgeeks.org/topological-sorting-indegree-based-solution/ (Kahn’s algorithm for Topological Sorting)
+ */
+public class _1_TopologicalSort_BFS {
+
+    public static void main(String[] args) {
+
+        _1_TopologicalSort_BFS obj = new _1_TopologicalSort_BFS();
+
+        int[][] edges = {{3, 0}, {1, 0}, {2, 0}};
+
+        List<Integer> topoSort = obj.topoSort(4, edges);
+        for (int i : topoSort) {
+            System.out.println(i + " ");
+        }
+    }
+
+    /*
+        The idea is to use BFS to find the topological sort of the graph.
+        This is called Kahn's algorithm.
+     */
+    public List<Integer> topoSort(int v, int[][] edges) {
+
+        int[] inDegrees = new int[v];
+        List<List<Integer>> adjList = getAdjList(v, edges, inDegrees);
+
+        Queue<Integer> queue = new LinkedList<>();
+
+        for (int i = 0; i < inDegrees.length; i++) {
+            if (inDegrees[i] == 0) {
+                queue.offer(i);
+            }
+        }
+
+        List<Integer> topoSortedNodes = new ArrayList<>();
+
+        while (!queue.isEmpty()) {
+
+            int currNode = queue.poll();
+            topoSortedNodes.add(currNode);
+
+            for (int adjNode : adjList.get(currNode)) {
+
+                inDegrees[adjNode] -= 1;
+
+                if (inDegrees[adjNode] == 0) {
+                    queue.add(adjNode);
+                }
+            }
+        }
+
+        return topoSortedNodes;
+    }
+
+    /*
+        This method
+        1. will create adjacency list and
+        2. will populate inDegrees array
+     */
+    private List<List<Integer>> getAdjList(int v, int[][] edges, int[] inDegrees) {
+
+        List<List<Integer>> adjList = new ArrayList<>();
+        for (int i = 0; i < v; i++) {
+            adjList.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < edges.length; i++) {
+
+            int src = edges[i][0];
+            int dst = edges[i][1];
+
+            adjList.get(src).add(dst);
+
+            inDegrees[dst] += 1;
+        }
+
+        return adjList;
+    }
+}
---
+++ graph/src/topo_sort/basics/_2_TopologicalSort_DFS.java
@@ -0,0 +1,82 @@
+package src.topo_sort.basics;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * https://www.geeksforgeeks.org/problems/topological-sort/1
+ * <p>
+ * https://www.youtube.com/watch?v=5lZ0iJMrUMk&ab_channel=takeUforward (Striver)
+ * https://github.com/sushovankarmakar/GeeksforGeeks/blob/master/3.%20Medium/src/Graph_TopologicalSort.java
+ */
+public class _2_TopologicalSort_DFS {
+
+    public static void main(String[] args) {
+
+        _2_TopologicalSort_DFS obj = new _2_TopologicalSort_DFS();
+
+        int[][] edges = {{3, 0}, {1, 0}, {2, 0}};
+
+        List<Integer> topoSort = obj.topoSort(4, edges);
+        for (int i : topoSort) {
+            System.out.println(i + " ");
+        }
+    }
+
+    public ArrayList<Integer> topoSort(int v, int[][] edges) {
+
+        List<List<Integer>> adjList = getAdjList(v, edges);
+
+        Stack<Integer> stack = new Stack<>();
+        boolean[] isVisited = new boolean[v];
+
+        for (int i = 0; i < v; i++) {
+
+            if (!isVisited[i]) {
+                dfs(i, adjList, isVisited, stack);
+            }
+        }
+
+        ArrayList<Integer> topoSortedNodes = new ArrayList<>();
+
+        while (!stack.isEmpty()) {
+            topoSortedNodes.add(stack.pop());
+        }
+
+        return topoSortedNodes;
+    }
+
+    private void dfs(int currNode, List<List<Integer>> adjList, boolean[] isVisited, Stack<Integer> stack) {
+
+        isVisited[currNode] = true;
+
+        for (int adjNode : adjList.get(currNode)) {
+
+            if (!isVisited[adjNode]) {
+
+                dfs(adjNode, adjList, isVisited, stack);
+            }
+        }
+
+        stack.push(currNode);
+    }
+
+    private List<List<Integer>> getAdjList(int v, int[][] edges) {
+
+        List<List<Integer>> adjList = new ArrayList<>();
+        for (int i = 0; i < v; i++) {
+            adjList.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < edges.length; i++) {
+
+            int src = edges[i][0];
+            int dst = edges[i][1];
+
+            adjList.get(src).add(dst);
+        }
+
+        return adjList;
+    }
+}
