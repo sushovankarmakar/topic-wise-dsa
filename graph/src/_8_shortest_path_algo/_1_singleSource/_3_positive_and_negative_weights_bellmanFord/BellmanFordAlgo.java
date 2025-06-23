30a57a5cfcd9bc6a8149c480bb9e7f8c4794533d
Sushovan Karmakar <sushovankarmakar1996@gmail.com>
2025-06-23 18:48:57 +0530

    Graph: Shortest Path Algo: Bellman Ford: positive and negative weights graph

--- graph/src/_8_shortest_path_algo/_1_singleSource/_3_positive_and_negative_weights_bellmanFord/BellmanFordAlgo.java
+++ graph/src/_8_shortest_path_algo/_1_singleSource/_3_positive_and_negative_weights_bellmanFord/BellmanFordAlgo.java
@@ -1,37 +1,14 @@
-package src.shortest_path_algo;
+package src._8_shortest_path_algo._1_singleSource._3_positive_and_negative_weights_bellmanFord;
 
 import java.util.Arrays;
-import java.util.List;
 
 /**
+ * https://www.youtube.com/watch?v=Hcff0gC1VJg&ab_channel=AdityaVerma
+ * <p>
  * https://www.geeksforgeeks.org/problems/distance-from-the-source-bellman-ford-algorithm/1 (GFG)
  * https://www.youtube.com/watch?v=0vVofAhAYjc&ab_channel=takeUforward (Striver)
  */
 public class BellmanFordAlgo {
-
-    public static void main(String[] args) {
-
-        int v = 6;
-        List<List<Integer>> edges = Arrays.asList(
-                Arrays.asList(3, 2, 6),
-                Arrays.asList(5, 3, 1),
-                Arrays.asList(0, 1, 5),
-                Arrays.asList(1, 5, -3),
-                Arrays.asList(1, 2, -2),
-                Arrays.asList(3, 4, -2),
-                Arrays.asList(2, 4, 3)
-        );
-
-        int[] dist = bellmanFordAlgo(v, edges, 0); // output -> dist [0, 5, 3, 1, 6, 4]
-
-        if (dist[0] == -1) {
-            System.out.println("Negative cycle detected");
-        } else {
-            for (int i = 0; i < v; i++) {
-                System.out.println("Shortest distance from 0 to " + i + " is " + dist[i]);
-            }
-        }
-    }
 
     /**
      * Bellman Ford Algorithm
@@ -49,52 +26,77 @@ public class BellmanFordAlgo {
      * Time Complexity: O(V * E)
      * Space Complexity: O(V)
      */
-    private static int[] bellmanFordAlgo(int v, List<List<Integer>> edges, int start) {
+    private static int[] bellmanFordAlgo(int v, int[][] edges, int src) {
 
         int[] dist = new int[v];
         Arrays.fill(dist, 100000000);
-        dist[start] = 0;
+        dist[src] = 0;
 
         /**
          * V-1 times relaxation is enough to find the shortest path
-         * because the shortest path can have at most V-1 edges
+         * because the shortest path can have at most V-1 edges,
          * so, if we relax all the edges V-1 times, then we will get the shortest path
          *
          * if we relax the edges V times and still the distance is getting updated, then there is a negative cycle
          */
 
-        // relaxation of edges start here
+        // (V-1)th relaxation of edges starts here
         for (int i = 0; i < v - 1; i++) {
 
-            for (List<Integer> edge : edges) {
+            for (int[] edge : edges) {
 
-                int src = edge.get(0);
-                int dst = edge.get(1);
-                int wt = edge.get(2);
+                int srcNode = edge[0];
+                int dstNode = edge[1];
+                int weight = edge[2];
 
                 // For each edge (u, v) with weight w,
                 // if the distance to v through u is shorter than the current distance to v,
                 // update the distance to v.
-                if (dist[src] != 100000000 && (dist[src] + wt < dist[dst])) {
+                if (dist[srcNode] != 100000000 && (dist[srcNode] + weight < dist[dstNode])) {
 
-                    dist[dst] = dist[src] + wt;
+                    dist[dstNode] = dist[srcNode] + weight;
                 }
             }
         }
 
-        // Nth relaxation to check negative cycle
-        for (List<Integer> edge : edges) {
+        // Vth relaxation to check negative cycle
+        for (int[] edge : edges) {
 
-            int src = edge.get(0);
-            int dst = edge.get(1);
-            int wt = edge.get(2);
+            int srcNode = edge[0];
+            int dstNode = edge[1];
+            int weight = edge[2];
 
-            if (dist[src] != 100000000 && (dist[src] + wt < dist[dst])) {
+            if (dist[srcNode] != 100000000 && (dist[srcNode] + weight < dist[dstNode])) {
 
                 return new int[]{-1};
             }
         }
 
         return dist;
+    }
+
+
+    public static void main(String[] args) {
+
+        int v = 6;
+        int[][] edges = {
+                {3, 2, 6},
+                {5, 3, 1},
+                {0, 1, 5},
+                {1, 5, -3},
+                {1, 2, -2},
+                {3, 4, -2},
+                {2, 4, 3}
+        };
+
+        int[] dist = bellmanFordAlgo(v, edges, 0); // output -> dist [0, 5, 3, 1, 6, 4]
+
+        if (dist[0] == -1) {
+            System.out.println("Negative cycle detected");
+        } else {
+            for (int i = 0; i < v; i++) {
+                System.out.println("Shortest distance from 0 to " + i + " is " + dist[i]);
+            }
+        }
     }
 }
