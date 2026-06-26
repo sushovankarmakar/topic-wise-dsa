87f51029cef38afdd52385d3f0be1cb83666c054
Sushovan Karmakar <sushovankarmakar1996@gmail.com>
2026-06-26 12:30:45 +0530

    Topological Sort DFS

--- graph/src/_6_topo_sort/_1_basics/TopologicalSort_DFS.java
+++ graph/src/_6_topo_sort/_1_basics/TopologicalSort_DFS.java
@@ -1,4 +1,4 @@
-package src._5_topo_sort._1_basics;
+package src._6_topo_sort._1_basics;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,11 +10,11 @@ import java.util.Stack;
  * https://www.youtube.com/watch?v=5lZ0iJMrUMk&ab_channel=takeUforward (Striver)
  * https://github.com/sushovankarmakar/GeeksforGeeks/blob/master/3.%20Medium/src/Graph_TopologicalSort.java
  */
-public class _2_TopologicalSort_DFS {
+public class TopologicalSort_DFS {
 
     public static void main(String[] args) {
 
-        _2_TopologicalSort_DFS obj = new _2_TopologicalSort_DFS();
+        TopologicalSort_DFS obj = new TopologicalSort_DFS();
 
         int[][] edges = {{3, 0}, {1, 0}, {2, 0}};
 
@@ -26,57 +26,53 @@ public class _2_TopologicalSort_DFS {
 
     public ArrayList<Integer> topoSort(int v, int[][] edges) {
 
-        List<List<Integer>> adjList = getAdjList(v, edges);
-
-        Stack<Integer> stack = new Stack<>();
+        List<List<Integer>> adjList = getAdjList(v, edges, true); // adj list from edges
+        Stack<Integer> stack = new Stack<>(); // storing topo sort output
         boolean[] isVisited = new boolean[v];
 
+        // there can be multiple components, use for loop to visit all the components
         for (int i = 0; i < v; i++) {
-
             if (!isVisited[i]) {
                 dfs(i, adjList, isVisited, stack);
             }
         }
 
+        // topo sort is stored in stack, return it in list form
         ArrayList<Integer> topoSortedNodes = new ArrayList<>();
-
         while (!stack.isEmpty()) {
             topoSortedNodes.add(stack.pop());
         }
-
         return topoSortedNodes;
     }
 
     private void dfs(int currNode, List<List<Integer>> adjList, boolean[] isVisited, Stack<Integer> stack) {
-
         isVisited[currNode] = true;
 
         for (int adjNode : adjList.get(currNode)) {
-
             if (!isVisited[adjNode]) {
-
                 dfs(adjNode, adjList, isVisited, stack);
             }
         }
-
-        stack.push(currNode);
+        stack.push(currNode); // all the adj nodes are visited, safe to add to stack.
     }
 
-    private List<List<Integer>> getAdjList(int v, int[][] edges) {
+    private List<List<Integer>> getAdjList(int V, int[][] edges, boolean isDirected) {
 
         List<List<Integer>> adjList = new ArrayList<>();
-        for (int i = 0; i < v; i++) {
+        for (int i = 0; i < V; i++) {
             adjList.add(new ArrayList<>());
         }
 
-        for (int i = 0; i < edges.length; i++) {
+        for (int[] edge : edges) {
 
-            int src = edges[i][0];
-            int dst = edges[i][1];
+            int src = edge[0];
+            int dst = edge[1];
 
             adjList.get(src).add(dst);
+            if (!isDirected) {
+                adjList.get(dst).add(src);
+            }
         }
-
         return adjList;
     }
 }
